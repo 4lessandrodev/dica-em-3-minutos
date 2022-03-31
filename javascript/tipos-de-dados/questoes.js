3712f64b1d0e102f83712a28be296b852c31b574
4lessandrodev <alessandroadm@live.com>
2022-03-30 21:37:17 -0300

    feat(number): added exercicie about numbers

--- javascript/tipos-de-dados/questoes.js
+++ javascript/tipos-de-dados/questoes.js
@@ -43,3 +43,30 @@ limpar();
 // A: [] true, true, true, null
 // B: [] false, true, false, NaN
 // C: [] true, false, true, 42
+
+
+
+// -----------------------------------------------
+limpar();
+
+{
+	var result = 0.3 + 0.2;
+
+	imprimir(result);
+
+	var result = Number.parseFloat('20.10O3ABC');
+
+	imprimir(result);
+
+	var valor = 9.801;
+
+	var result = valor.toPrecision(2);
+
+	imprimir(result);
+}
+
+// O que será exibido no console?
+// Marque sua resposta
+// A: [] 0.30, 20.100, 9.80
+// B: [] 0.3, 20.10, 9.801
+// C: [] 0.30000000000000004, 20.1, 9.8
